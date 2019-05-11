1a85c4d3a0aee98d4dde23f3e0ccb7c3e970b472
ZhangZhenfang <1228436617@qq.com>
2019-05-11 15:01:12 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
+        System.out.println(Arrays.toString(args));
         SpringApplication.run(DemoApplication.class, args);
     }
 
