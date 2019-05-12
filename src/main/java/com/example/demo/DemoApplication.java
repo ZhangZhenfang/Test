afc8607b46c477b43c06449a513932ac62487b12
ZhangZhenfang <1228436617@qq.com>
2019-05-12 14:11:34 +0800

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -10,6 +10,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
         System.out.println(Arrays.toString(args));
+        System.out.println("!!!!!!!!!!!!!!!!!!!!!!");
         SpringApplication.run(DemoApplication.class, args);
     }
 
